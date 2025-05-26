18a3f1a45a081137b2d14c386f0f407dd62445b5
EtielOguh <hugodsleite@gmail.com>
2025-05-26 11:05:56 -0300

    Minimamente ajustado

--- itens/shields.py
+++
@@ -1,5 +0,0 @@
-from .itens_base import Itens
-
-class shield():
-    def __init__(self):
-        super().__init__(name= "Shield Of Stone", attack = 0, defense = 10, type = 1)
--- itens/swords.py
+++
@@ -1,5 +0,0 @@
-from .itens_base import Itens
-
-class Sword():
-    def __init__(self):
-        super().__init__(name= "Sword of Stone", attack = 10, defense = 0, type = 2)
